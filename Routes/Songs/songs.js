8f40cf8f71406fa750170f08802b25d8cedb2bcb
mateo0203 <mateo03rg@gmail.com>
2023-02-11 02:11:49 -0500

    Routes

---
+++ Routes/Songs/songs.js
@@ -0,0 +1,7 @@
+const Router = require("express").Router()
+
+//Get liked songs
+
+//Post add song to tag from user
+
+//Post remove song from tag to user
---
+++ Routes/Tags/tags.js
@@ -0,0 +1,9 @@
+const Router = require("express").Router()
+
+
+//get all tags from user
+
+
+//Get all songs with tag from user
+
+
