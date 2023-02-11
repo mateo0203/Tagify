6b7026f031116eef5ff67d471990a26900129611
Gero Gorostiaga <gerogorostiaga@lawn-128-61-47-169.lawn.gatech.edu>
2023-02-11 15:13:12 -0500

    gero

--- Routes/Songs/songs.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express").Router()
-
-//Get liked songs
-
-//Post add song to tag from user
-
-//Post remove song from tag to user
--- Routes/Tags/tags.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router()
-
-
-//get all tags from user
-
-
-//Get all songs with tag from user
-
-
