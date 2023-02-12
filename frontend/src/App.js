b5f6fb75180463f3d751c84e0373ac6a0050f69a
Gero Gorostiaga <gerogorostiaga@lawn-128-61-47-169.lawn.gatech.edu>
2023-02-11 23:01:07 -0500

    front nuevo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,62 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import './style.css'; // Import the CSS file
+import { Container, Row, Col } from "react-bootstrap";
 import StockTable from "./StockTable";
 
 const products = [];
 
 function addProducts(quantity) {
-  const startId = products.length;
+  const titlestring = "Title"
+  const artiststring = "Artist"
+  const albumstring = "Album"
+  const tagsstring = "Tag"
   for (let i = 0; i < quantity; i++) {
-    const id = startId + i;
     products.push({
-      id: id,
-      name: Math.random().toString(36).substr(2).toUpperCase(),
-      company: ["Apple", "Google", "Tesla", "Orion"][
-        Math.floor(Math.random() * 4)
-      ],
-      quantity: Math.floor(Math.random() * 24),
-      isInStock: ["yes", "no", "N/A"][Math.floor(Math.random() * 3)]
+      title: (titlestring + " " + String(i)),
+      artist: (artiststring + " " + String(i)),
+      album: (albumstring + " " + String(i)),
+      tags: (tagsstring + " " + String(i)),
     });
   }
 }
 
+
 addProducts(100);
+
 
 const App = () => {
   return (
-    <Container>
-      <h1 align="center">Bootstrap Table</h1>
+    <Container fluid>
+      <Row>
+        <Col xs={2}>
+          <Sidebar/>
+        </Col>
+        <Col xs={10}>
+          <MainContent />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="bg-white p-3">
+      <h3>Playlists</h3>
+      <p>This is the sidebar content</p>
+    </div>
+  );
+}
+
+
+function MainContent() {
+  return (
+    <Container className="custom-background">
+      <h1 className="mt-3 text-center custom-font">Tagify</h1>
       <hr />
       <StockTable products={products} />
     </Container>
   );
-};
+}
 
 export default App;
