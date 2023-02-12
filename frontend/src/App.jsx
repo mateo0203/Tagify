818071cc6253a57ec480a9a1e21a011a114523c9
augustochang <123024916+augustochang@users.noreply.github.com>
2023-02-11 23:21:19 -0500

    jsx login page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1></h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
